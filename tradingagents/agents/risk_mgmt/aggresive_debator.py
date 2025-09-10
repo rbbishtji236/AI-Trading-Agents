e3a6690143243d9a0d4566722ffca1441da248c6
Rohit Bisht <rbbishtji236@gmail.com>
2025-09-10 23:33:10 +0530

    add risk_mgmt file

---
+++ tradingagents/agents/risk_mgmt/aggresive_debator.py
@@ -0,0 +1,21 @@
+import time
+import json
+
+
+def create_risky_debator(llm):
+    def risky_node(state) -> dict:
+        risk_debate_state = state["risk_debate_state"]
+        history = risk_debate_state.get("history", "")
+        risky_history = risk_debate_state.get("risky_history", "")
+
+        current_safe_response = risk_debate_state.get("current_safe_response", "")
+        current_neutral_response = risk_debate_state.get("current_neutral_response", "")
+
+        market_research_report = state["market_report"]
+        sentiment_report = state["sentiment_report"]
+        news_report = state["news_report"]
+        fundamentals_report = state["fundamentals_report"]
+
+        trader_decision = state["trader_investment_plan"]
+
+        
